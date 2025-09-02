1da411a7002547462fa1f4ddbf2b5832ca2d7afb
Zakisanaa <justinlloydballesteros27@gmail.com>
2025-09-02 20:01:26 +0800

    Delete rowandcolumn.py

--- rowandcolumn.py
+++
@@ -1,7 +0,0 @@
-rows = int(input("Enter rows: "))
-cols = int(input("Enter columns: "))
-
-for i in range(rows):
-    for j in range(cols):
-        val = input(f"Value at ({i+1},{j+1}): ")
-        print(f"You entered: {val}")
